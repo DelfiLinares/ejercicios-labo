2a0b762b490c446c76344cb6f93820fc6df56dd1
DelfiLinares <alumno26.linares.pataro.delfina@ipm.edu.ar>
2024-04-18 11:46:18 -0300

    definimos variables

---
+++ ej1-3.stringcoma.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string palabra;
+    int pos = 0;
+
+    cout<<"Ingrese palabra"<<endl;
+    cin>>palabra;
+
+    if (palabra[pos] == ','){
+        
+    }
+}
